cb094e7cbe2e0274cd4a24561b5cd24fa663668b
ahrana <ahrana@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-04-07 17:22:49 +0000

    git-svn-id: https://plugins.svn.wordpress.org/js-contact-form/trunk@528629 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ Js Contact Form.php
@@ -0,0 +1,129 @@
+<?php
+/*
+Plugin Name: Js Contact Form
+Plugin URI: http://bdweblab.com/plugins/js-contact-form/
+Description: Creative Wordpress Contact Form with attachment support and also include Empty Form validation.</br>At first go to plugin editor and (Js Contact Form.php)findout 'yourname@mail.com' then Replace This Email Address.Now create a new page at your wordpress site and insert this code[contact_form]
+Author: Cx Rana
+Version: 1.0
+Author URI: http://bdweblab.com/about-us
+*/
+
+function contact_form_markup() {
+
+$form_action    = get_permalink();
+$author_default = $_COOKIE['comment_author_'.COOKIEHASH];
+$email_default  = $_COOKIE['comment_author_email_'.COOKIEHASH];
+
+if ( ($_SESSION['contact_form_success']) ) {
+$contact_form_success = '<p style="color: green">Thank your for Your Feedback.</p>';
+unset($_SESSION['contact_form_success']);
+}
+
+$markup = <<<EOT
+
+<div id="commentform">
+
+	{$contact_form_success}
+     
+   <form onsubmit="return validateForm(this);" action="{$form_action}" method="post" enctype="multipart/form-data" style="text-align: left">
+   
+   <p><input type="text" name="author" id="author" value="{$author_default}" size="22" /> <label for="author">Your Name *</label></p>
+   <p><input type="text" name="email" id="email" value="{$email_default}" size="22" /> <label for="email">E mail *</label></p>
+   <p><input type="text" name="subject" id="subject" value="" size="22" /> <label for="subject">Subject *</label></p>
+   <p><textarea name="message" id="message" cols="100%" rows="10">Please type here...</textarea></p>
+   <p><label for="attachment"><strong>File/photos </strong></label> <input type="file" name="attachment" id="attachment" /></p>
+   <p><input name="send" type="submit" id="send" value="Send" /></p>
+   
+   <input type="hidden" name="contact_form_submitted" value="1">
+   
+   </form>
+   
+</div>
+
+EOT;
+
+return $markup;
+
+}
+
+add_shortcode('contact_form', 'contact_form_markup');
+
+function contact_form_process() {
+
+session_start();
+
+ if ( !isset($_POST['contact_form_submitted']) ) return;
+
+ $author  = ( isset($_POST['author']) )  ? trim(strip_tags($_POST['author'])) : null;
+ $email   = ( isset($_POST['email']) )   ? trim(strip_tags($_POST['email'])) : null;
+ $subject = ( isset($_POST['subject']) ) ? trim(strip_tags($_POST['subject'])) : null;
+ $message = ( isset($_POST['message']) ) ? trim(strip_tags($_POST['message'])) : null;
+
+ if ( $author == '' ) wp_die('Error 1: Add your Name Please.'); 
+ if ( !is_email($email) ) wp_die('Error 2: Type your Email Please.');
+ if ( $subject == '' ) wp_die('Error 3: Add a Subject First.');
+ 
+ //we will add e-mail sending support here soon
+ 
+require_once ABSPATH . WPINC . '/class-phpmailer.php';
+$mail_to_send = new PHPMailer();
+
+$mail_to_send->FromName = $author;
+$mail_to_send->From     = $email;
+$mail_to_send->Subject  = $subject;
+$mail_to_send->Body     = $message;
+
+$mail_to_send->AddReplyTo($email);
+$mail_to_send->AddAddress('yourname@mail.com'); //contact form destination e-mail
+
+if ( !$_FILES['attachment']['error'] == 4 ) { //something was send
+	
+	if ( $_FILES['attachment']['error'] == 0 && is_uploaded_file($_FILES['attachment']['tmp_name']) )
+	
+		$mail_to_send->AddAttachment($_FILES['attachment']['tmp_name'], $_FILES['attachment']['name']);
+	
+	else 
+		
+		wp_die('Error: Somethimg was wrong(Please Try again later)');
+		
+}
+
+if ( !$mail_to_send->Send() ) wp_die('Error : Mail Sending Failed - Js Contact Form : ' . $mail_to_send->ErrorInfo);
+
+$_SESSION['contact_form_success'] = 1;
+
+ 
+ header('Location: ' . $_SERVER['HTTP_REFERER']);
+ exit();
+
+} 
+
+add_action('init', 'contact_form_process');
+
+function contact_form_js() { ?>
+
+<script type="text/javascript">
+function validateForm(form) {
+
+	var errors = '';
+	var regexpEmail = /\w{1,}[@][\w\-]{1,}([.]([\w\-]{1,})){1,3}$/;
+		
+	if (!form.author.value) errors += "Warning 1 : Name box empty.\n";
+	if (!regexpEmail.test(form.email.value)) errors += "Warning 2 : Email box empty.\n";
+	if (!form.subject.value) errors += "Warning 3 : Subject.\n";
+
+	if (errors != '') {
+		alert(errors);
+		return false;
+	}
+	
+return true;
+	
+}
+</script>
+
+<?php }
+
+add_action('wp_head', 'contact_form_js');
+
+?>
---
+++ readme.txt
@@ -0,0 +1,78 @@
+=== Js Contact Form ===
+Contributors: Cx Rana
+Developer link: https://bdweblab.com/about-us
+Tags:  Js Contact Form, Wordpress contact form, java scripts wordpress contact plugin, wordpress contact plugin, Wordpress Plugin,,contact page, Cx Rana, bdweblab ,Simple php contact form
+Requires at least: 2.3
+Tested up to: 3.3.1
+Stable tag: trunk
+
+Creative Wordpress Contact Form with attachment support and also include Empty Form validation.At first go to plugin editor and (Js Contact Form.php)findout 'yourname@mail.com' then Replace This Email Address.Now create a new page at your wordpress site and insert this code [contact_form]
+
+== Description == 
+Creative Wordpress Contact Form with attachment support and also include Empty Form validation. at your wordpress site and insert this code[contact_form]
+after installation then go to your Plugin Editor and findout 'yourname@mail.com' and replace it for ypur own mailing.
+Now go to addnew page>Create new pages>paste this code  [contact_form] 
+
+= Features =
+* Simple Contact Systems for Your Wordpress Blog</br>
+* Java Scripts Support validation Form</br>
+* Empty Form validation 
+* File/Photos Attachment
+
+
+Plugin by Cx Rana. Need a <a href="http://www.bdweblab.com/about-us">Web Designer</a>?
+
+== Changelog ==
+
+= 1.0 = 
+* WordPress 3.3.1+ approved
+
+
+== Installation ==
+
+1. Upload the whole plugin folder to your /wp-content/plugins/ folder.
+2. Go to the Plugins page and activate the plugin.
+3. Go to your plugin editor and find 'yourname@mail.com',now replace this email
+4. Now create a new pages and paste this code [contact_form]
+
+
+
+== Screenshots ==
+
+1. Js Contact Form Demo
+2. Developer Site
+
+== License ==
+
+This file is part of Js Contact Form.
+
+Js Contact Form is free Plugin: you don't modify it,under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+Js Contact Form is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with WP Wall. If not, see <http://www.gnu.org/licenses/>.
+
+
+== Frequently Asked Questions ==
+
+= How does it work? =
+
+It creates a Contact page in your blog under which some config. 
+
+= What if I don't show form, how do I code this to appear in the Contact Page?
+
+Just put this code contact page in your addnew page,
+
+[contact_form]
+
+= How can I Replace contact form destination e-mail ?
+
+Now Go To plugin editor and find 'yourname@mail.com' Please replace this e mail address.
+
+= Can I suggest an feature for the plugin? =
+
+Of course, visit <a href="http://bdweblab.com/plugins/js-contact-form/">Js Contact Form Home Page</a>
+
+= I like your work, are you available for hire? =
+
+Yes I am, visit my <a href="http://www.bdweblab.com/about-us">Cx Rana's Official Page</a> page to find out more.
